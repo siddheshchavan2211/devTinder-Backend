f0a567a90163a067404366f8ac7b8d7e02b95bde
siddheshchavan2211 <siddheshchavan02@gmail.com>
2025-01-11 20:29:08 +0530

    added feed api

--- src/routes/getUser.js
+++ src/routes/getUser.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { UserAuthorization } = require("../middleware/Authorization");
 const ConnReqModel = require("../models/connRequest");
+const User = require("../models/user");
 //getting all pending request from user
 userRouter.get(
   "/user/requests/received",
@@ -48,6 +49,35 @@ userRouter.get("/user/friends", UserAuthorization, async (req, res) => {
       return friend.senderId;
     });
     res.json({ message: "friends", data: data });
+  } catch (err) {
+    res.status(400).send("Error " + err.message);
+  }
+});
+
+userRouter.get("/feed", UserAuthorization, async (req, res) => {
+  try {
+    const user = req.user;
+    //get all friends except current user and friends and rejected requests
+    //first finding all connection request which i sent or recived then select only senderid and recieverid
+    const friends = await ConnReqModel.find({
+      $or: [{ senderId: user._id }, { recieverId: user._id }],
+    }).select("senderId recieverId");
+    //hiding the user which are friend and self
+    const hideUsers = new Set();
+    friends.forEach((friend) => {
+      hideUsers.add(friend.senderId.toString());
+      hideUsers.add(friend.recieverId.toString());
+    });
+    //using $nin(notin) to pass this users and print rest values
+    const feedUsers = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsers) } },
+        {
+          _id: { $ne: user._id },
+        },
+      ],
+    }).select("firstName lastName photoUrl skills about age gender mobile");
+    res.json({ message: "feed", data: feedUsers });
   } catch (err) {
     res.status(400).send("Error " + err.message);
   }
